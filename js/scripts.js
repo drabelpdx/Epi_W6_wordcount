1221f8742fd4e416fc3a143eefcab7fe8021e89e
drabelpdx <drabelpdx@gmail.com>
2015-05-28 19:55:41 -0700

    Work from home. Word count functions as expected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,36 @@
 var wordCount = function(sentence) {
   var arr = sentence.split(" ");
-    var a = [], b = [], prev;
-    arr.sort();
+    var words = [], counts = [], prev;
+      arr.sort();
     for ( var i = 0; i < arr.length; i++ ) {
-        if ( arr[i] !== prev ) {
-            a.push(arr[i]);
-            b.push(1);
-        } else {
-            b[b.length-1]++;
-        }
+      if ( arr[i] !== prev ) {
+        words.push(arr[i]);
+        counts.push(1);
+      } else {
+        counts[counts.length-1]++;
+      }
         prev = arr[i];
     }
+      var empty = [];
 
-    wordSorter([a, b]);
-    //var result = [a, b];
+      for (var i = 0; i < words.length; i ++) {
+         empty.push(counts[i] + " : " + words[i] + "<br />");
+         empty.sort().reverse();
+      }
+         empty.toString();
+      return empty;
 };
 
-    var wordSorter = function(result) {
-    var words = result[0];
-    var counts = result[1];
-    var empty = [];
+  $(document).ready(function() {
+    $("form#wordcount").submit(function(event) {
+      var sentence = $("input#words").val();
+      var result = wordCount(sentence);
+      console.log(sentence)
+      console.log(result)
+      $(".words").html(result);
 
-    for (var i = 0; i < words.length; i ++) {
-      empty.push(counts[i] + ":" + words[i]);
-      empty.sort().reverse();
+      $("#result").show();
 
-      }
-      return empty;
-    };
+      event.preventDefault();
+    });
+  });
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
  describe('wordCount', function() {
 
   it("returns a count and list of words", function() {
-    expect(wordCount("It is a mad mad mad world")).to.eql([a, b]);
+    expect(wordCount("It is a mad mad mad world")).to.eql(["3:mad", "1:world", "1:is", "1:a", "1:It"]);
   });
 });
